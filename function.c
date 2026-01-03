00008320c667b7e18149fec1cdee21b7b5b050e8
Semin_Rwai <hwangchei901@gmail.com>
2026-01-02 19:26:32 -0500

    input function

--- function.c
+++ function.c
@@ -14,3 +14,21 @@ void meow(int n)
         printf("meow\n");
     }
 }
+//input
+#include <cs50.h>
+#include <stdio.h>
+
+void meow(int n);
+
+int main(void)
+{
+    int n = get_int("What's n?");
+    meow(n);
+}
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
