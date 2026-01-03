0bcf5b7d81b519508598d6b1597801aa1fe8dcf8
Semin_Rwai <hwangchei901@gmail.com>
2026-01-02 19:27:42 -0500

    new function name

--- function.c
+++ function.c
@@ -18,14 +18,14 @@ void meow(int n)
 #include <cs50.h>
 #include <stdio.h>
 
-void meow(int n);
+void meow(int times);
 
 int main(void)
 {
     int n = get_int("What's n?");
     meow(n);
 }
-void meow(int n)
+void meow(int times)
 {
     for (int i = 0; i < n; i++)
     {
