42f003f3f4693163aeed64e629de7f9923db4494
Semin_Rwai <hwangchei901@gmail.com>
2026-01-02 19:25:07 -0500

    new function

--- function.c
+++ function.c
@@ -1,15 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void meow(void)
-{
-    printf("meow\n");
-}
+void meow(int n);
 
 int main(void)
 {
-    for (int i = 0; i<3; i++)
+    meow(3);
+}
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        meow();
+        printf("meow\n");
     }
 }
